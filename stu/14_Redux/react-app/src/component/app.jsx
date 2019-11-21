5a73ae06d17f7f3052ec8a8d4d175724a01ed6ff
sAMs126 <492457689@qq.com>
2019-11-21 19:09:17 +0800

    设置初始化状态

--- stu/14_Redux/react-app/src/component/app.jsx
+++ stu/14_Redux/react-app/src/component/app.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+    state = {
+        count: 0
+    };
+
     render() {
+        const { count } = this.state;
         return (
             <div>
-                <p>click 0 times</p>
+                <p>click {count} times</p>
                 <div>
                     <select>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                         <option value="4">4</option>
-                    </select>&nbsp;
+                    </select>
+                    &nbsp;
                     <button>+</button>&nbsp;
                     <button>-</button>&nbsp;
                     <button>add if odd</button>&nbsp;
