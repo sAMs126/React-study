4364b35d67f57a65817bb29888985b440d4f4d1c
sAMs126 <492457689@qq.com>
2019-11-25 22:18:40 +0800

    初始getState()、dispatch()、subscribe()

--- stu/14_Redux/react-redux-demo/src/component/app.jsx
+++ stu/14_Redux/react-redux-demo/src/component/app.jsx
@@ -1,49 +1,44 @@
 import React, { Component } from 'react';
+import { INCREMENT, DECREMENT } from '../redux/action-types';
 
 export default class App extends Component {
     state = {
         count: 0
     };
 
+    // 加
     add = () => {
-        // 1.获取增量值(string->number)
+        // 1. 获取增量值(string->number)
         const number = this.select.value * 1;
-        // 2.获取 count 当前值,并计算
-        const count = this.state.count + number;
-        // 3.更新 count 值
-        this.setState({
-            count
-        });
+        // 2. 调用 store 的 dispatch() 更新状态
+        this.props.store.dispatch({ type: INCREMENT, data: number });
     };
 
+    // 减
     sub = () => {
         const number = this.select.value * 1;
-        const count = this.state.count - number;
-        this.setState({
-            count
-        });
+        this.props.store.dispatch({ type: DECREMENT, data: number });
     };
 
+    // 奇数加
     addIfOdd = () => {
         const number = this.select.value * 1;
-        const count = this.state.count;
+        const count = this.props.store.getState();
         if (1 === count % 2) {
-            this.setState({
-                count: count + number
-            });
+            this.props.store.dispatch({ type: INCREMENT, data: number });
         }
     };
 
+    // 异步加
     addAsync = () => {
         const number = this.select.value * 1;
-        const count = this.state.count + number;
         setTimeout(() => {
-            this.setState({ count });
+            this.props.store.dispatch({ type: INCREMENT, data: number });
         }, 1000);
     };
 
     render() {
-        const { count } = this.state;
+        const count = this.props.store.getState();
         return (
             <div>
                 <p>click {count} times</p>
--- stu/14_Redux/react-redux-demo/src/index.jsx
+++ stu/14_Redux/react-redux-demo/src/index.jsx
@@ -4,10 +4,18 @@ import { createStore } from 'redux';
 import App from './component/app';
 import { counter } from './redux/reducers';
 
-
 // 生成一个 store 对象
 //  |-  第一次内部会调用 Reducer 函数得到初始 state
 const store = createStore(counter);
-console.log(store);
+console.log(store, store.getState());
 
-ReactDom.render(<App />, document.getElementById('root'));
+// 定义 render 函数
+function render() {
+    ReactDom.render(<App store={store} />, document.getElementById('root'));
+}
+
+// 初始化渲染
+render();
+
+// 订阅监听（store中状态发生变化，自动重绘）
+store.subscribe(render);
