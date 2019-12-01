81d6b3382cbdba6f45cbb2dc57a15847b09d0573
sAMs126 <492457689@qq.com>
2019-12-01 16:02:11 +0800

    使用 redux 调试工具

--- stu/14_Redux/react-redux-thunk/src/redux/store.js
+++ stu/14_Redux/react-redux-thunk/src/redux/store.js
@@ -1,13 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import { counter } from './reducers';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { counter } from './reducers';
 
 // 生成一个 store 对象
 //  |-  第一次内部会调用 Reducer 函数得到初始 state
 const store = createStore(
     counter,
     // 应用上异步中间件
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
 );
 // console.log(store, store.getState());
 
